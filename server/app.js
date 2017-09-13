04164a998dd1a70749a2f4a97948b83ddfd1a8de
Edge <baiban1994@gmail.com>
2017-09-13 19:57:28 +0800

    更换模板引擎

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var ejs = require('ejs');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,7 +14,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.html',ejs.__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
