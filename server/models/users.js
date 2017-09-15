0b3eb4373fe4ebda1b948f5347fb4e62d0e3d01c
Edge <baiban1994@gmail.com>
2017-09-15 18:12:35 +0800

    建立用户表模板初版

---
+++ server/models/users.js
@@ -0,0 +1,23 @@
+// 获取mongoose框架
+var mongoose = require('mongoose');
+// 建立模板
+var userSchema = new mongoose.schema({
+	'userId':String,
+	'userName':String,
+	'userPwd':String,
+	'orderList':Array,
+	'cartList':[
+		{
+			'productImage':String,
+			'productId':String,
+			'productName':String,
+			'productNum':String,
+			'salePrice':String,
+			'checked':String
+		}
+	],
+	'addressList':Array
+});
+
+// 导出模板
+module.exports = mongoose.model('User',userSchema,"users");
