488dfbf6261a96deb6200f5a46721a33d7afc6fe
Edge <baiban1994@gmail.com>
2017-09-16 15:34:02 +0800

    登出接口完成

--- server/routes/users.js
+++ server/routes/users.js
@@ -38,5 +38,16 @@ router.post('/login',(req,res,next)=>{
 	})
 })
 
-
+// 登出接口
+router.post('/logout',(req,res,next)=>{
+	res.cookie('userId','',{
+		path:'/',
+		maxAge:-1
+	});
+	res.json({
+		status:'0',
+		msg:'登出成功',
+		result:''
+	})
+})
 module.exports = router;
