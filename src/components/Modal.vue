9300b76386265a1d366796e92e0120c58f0d606b
Edge <baiban1994@gmail.com>
2017-09-16 22:58:17 +0800

    弹出框模块基本完成

---
+++ src/components/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+	<div class="md-modal modal-msg md-modal-transition" v-bind:class="{'md-show':mdShow}">
+          <div class="md-modal-inner">
+            <div class="md-top">
+              <button class="md-close" @click="closeModal">Close</button>
+            </div>
+            <div class="md-content">
+              <div class="confirm-tips">
+                <slot name='message'></slot>
+              </div>
+              <div class="btn-wrap">
+                <slot name='btnGroup'></slot>
+              </div>
+            </div>
+          </div>
+    </div>
+    <div class="md-overlay" v-if="mdShow" @click="mdShow=false"></div>
+</div>
+</template>
+
+<script>
+	/*
+	*==========================
+	* @Author: Edge
+	* @Version: 1.0
+	*==========================
+	*/
+export default{
+	props:[
+		'mdShow'
+	],
+	data(){
+		return{
+
+		};
+	},
+	methods:{
+		closeModal(){
+			this.$emit('close');
+		}
+	},
+	components:{
+
+	},
+	computed:{
+
+	}
+}
+</script>
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -7,7 +7,7 @@
 	    <div class="filter-nav">
 	      <span class="sortby">Sort by:</span>
 	      <a href="javascript:void(0)" class="default cur">Default</a>
-	      <a href="javascript:void(0)" class="price" @click='sortGoods()'>Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+	      <a href="javascript:void(0)" class="price" @click='sortGoods()'>Price <svg class="icon icon-arrow-short" :class="{'sort-up':!sortFlag}"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-arrow-short"></use></svg></a>
 	      <a href="javascript:void(0)" class="filterby stopPop" @click='showFilter'>Filter by</a>
 	    </div>
 	    <div class="accessory-result">
@@ -48,6 +48,22 @@
 	  </div>
 	</div>
 	<div class="md-overlay" v-show='overLayShow' @click='hideFilter'></div>
+	<modal :mdShow="mdShow" @close="closeModal">
+		<p slot='message'>请先登录</p>
+		<div slot='btnGroup'><a href='javascript:;' class="btn btn--m" @click='mdShow=false'>关闭</a></div>
+	</modal>
+	<modal :mdShow="mdShowCart" @close="closeModal">
+		<p slot='message'>
+			<svg class="icon-status-ok">
+	      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="icon-status-ok"></use>
+	    </svg>
+	    <span>加入购物车成功</span>
+    </p>
+		<div slot='btnGroup'>
+			<a href='javascript:;' class="btn btn--m" @click='mdShowCart=false'>继续购物</a>
+			<router-link href='javascript:;' class="btn btn--m" to='/cart'>查看购物车</router-link>
+		</div>
+	</modal>
 	<nav-footer></nav-footer>
 </div>
 </template>
@@ -65,6 +81,7 @@ import './../assets/css/product.css'
 import NavHeader from '@/components/Header.vue'
 import NavFooter from '@/components/Footer.vue'
 import NavBread from '@/components/Bread.vue'
+import Modal from '@/components/Modal.vue'
 
 import axios from 'axios'
 
@@ -99,7 +116,9 @@ export default{
 			sortFlag:true,
 			page:1,
 			pageSize:8,
-      busy:false
+      busy:false,
+      mdShow:false,
+      mdShowCart:false
 		};
 	},
 	methods:{
@@ -165,17 +184,22 @@ export default{
     		let res = response.data;
     		console.log(response)
     		if(res.status==0){
-    			alert('加入成功');
+    			this.mdShowCart=true;
     		}else{
-    			alert('error:'+ res.msg);
+    			this.mdShow = true;
     		}
     	})
+    },
+    closeModal(){
+    	this.mdShow=false;
+    	this.mdShowCart=false;
     }
 	},
 	components:{
 		NavHeader,
 		NavFooter,
-		NavBread
+		NavBread,
+		Modal
 	},
 	computed:{
 
