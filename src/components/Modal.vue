5f3f8f2ab1b630a4c8604aaba3e3fdef294444b0
Edge <baiban1994@gmail.com>
2017-09-17 10:14:22 +0800

    子组件不能直接修改父组件传递的数据，使用$emit触发父组件函数修改数据

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -15,7 +15,7 @@
             </div>
           </div>
     </div>
-    <div class="md-overlay" v-if="mdShow" @click="mdShow=false"></div>
+    <div class="md-overlay" v-if="mdShow" @click="closeModal"></div>
 </div>
 </template>
 
